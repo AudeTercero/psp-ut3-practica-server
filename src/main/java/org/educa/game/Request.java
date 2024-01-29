9cfca29ee1bcc26ddaf71f2f253a98c21ab99387
errin <errinostrich@gmail.com>
2024-01-29 16:32:37 +0100

    commit

--- src/main/java/org/educa/game/Request.java
+++ src/main/java/org/educa/game/Request.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Request implements Runnable {
-    private static int idMatch = 0;
+
     private Socket clientSocket;
     private int playersReady;
 
@@ -23,18 +23,22 @@ public class Request implements Runnable {
             String data = reader.readLine();
 
             String[] playerInfo = data.split(",");
-            System.out.println("hola");
-            System.out.println(playerInfo[0]);
 
             if (playerInfo.length == 3) {
                 String nick = playerInfo[0];
                 String gameType = playerInfo[1];
                 int playersNeeded = Integer.parseInt(playerInfo[2]);
+
                 String host = clientSocket.getInetAddress().getHostAddress();
                 int port = clientSocket.getPort();
+
                 Player player = new Player(nick, host, port);
+
                 System.out.println(player.getNickname());
-                Match match = newPlayerInMatch(player, gameType, playersNeeded);
+
+                Match match = Server.newPlayerInMatch(player, gameType, playersNeeded);
+                notifyAll();
+
                 out.writeObject(playerListInMatch(match));
                 out.flush();
                 //newPlayerInMatch(player, gameType, playersNeeded);
@@ -42,7 +46,7 @@ public class Request implements Runnable {
 
             } else {
                 int id = Integer.parseInt(playerInfo[0]);
-                endMatch(id);
+                Server.endMatch(id);
             }
 
 
@@ -51,70 +55,6 @@ public class Request implements Runnable {
         }
     }
 
-    public synchronized void waitForPlayers(int playersNeeded) throws InterruptedException {
-        playersReady++;
-        while (playersReady < playersNeeded) {
-            wait();
-        }
-    }
-
-
-    public synchronized Match newPlayerInMatch(Player player, String gameType, int playersNeeded) throws InterruptedException {
-        if (Server.matches.isEmpty()) {
-            Match match = new Match(plusCount(), gameType, playersNeeded);
-            player.setRoll(0);
-            modifyMatch(match,player);
-            modifyListMatch(match);
-            while (!match.isMatchFull()) {
-                System.out.println("Esperando jugadores1");
-                //wait();
-                Thread.sleep(1000);
-            }
-            System.out.println("Dejando de esperar");
-            notifyAll();
-            return match;
-        } else {
-            boolean isFull = true;
-
-            for (Match match : Server.matches.values()) {
-                if (match.getGameType().equalsIgnoreCase(gameType) && !match.isMatchFull()) {
-                    player.setRoll(1);
-                    modifyMatch(match,player);
-                    isFull = false;
-                    while (!match.isMatchFull()) {
-                        System.out.println("Esperando jugadores2");
-                        //wait();
-                        Thread.sleep(1000);
-                    }
-                    notifyAll();
-                    return match;
-                }
-
-            }
-
-            if (isFull) {
-                Match match = new Match(plusCount(), gameType, playersNeeded);
-                player.setRoll(0);
-                modifyMatch(match,player);
-                modifyListMatch(match);
-                while (!match.isMatchFull()) {
-                    System.out.println("Esperando jugadores3");
-                    //wait();
-                    Thread.sleep(1000);
-                }
-                notifyAll();
-                return match;
-            }
-        }
-        return null;
-
-    }
-    private synchronized void modifyListMatch(Match match){
-        Server.matches.put(match.getId(), match);
-    }
-    private synchronized void modifyMatch(Match match,Player player){
-        match.getPlayers().add(player);
-    }
 
     private ArrayList<String> playerListInMatch(Match match) {
         ArrayList<String> playerList = new ArrayList<>();
@@ -124,12 +64,5 @@ public class Request implements Runnable {
         return playerList;
     }
 
-    public synchronized void endMatch(int id) {
-        Server.matches.remove(id);
-    }
 
-
-    public synchronized int plusCount() {
-        return idMatch++;
-    }
 }
--- src/main/java/org/educa/game/Server.java
+++ src/main/java/org/educa/game/Server.java
@@ -10,10 +10,10 @@ import java.util.Map;
 
 
 public class Server {
-
+    private static int idMatch = 0;
     private final String HOST = "localhost";
     private final int PORT = 5555;
-    protected static Map<Integer, Match> matches = new HashMap<>();
+    protected final static Map<Integer, Match> matches = new HashMap<>();
     protected static List<Player> playersWaiting;
 
     public void run() {
@@ -38,6 +38,84 @@ public class Server {
             e.printStackTrace();
         }
 
+    }
+
+
+    public static synchronized Match newPlayerInMatch(Player player, String gameType, int playersNeeded) throws InterruptedException {
+
+        if (Server.matches.isEmpty()) {//Si no hay partidas
+            Match match = new Match(plusCount(), gameType, playersNeeded);
+            player.setRoll(0);
+            modifyMatch(match, player);
+            modifyListMatch(match);
+
+
+
+            System.out.println("Esperando jugadores1");
+
+
+            while (!match.isMatchFull()) {
+                //wait();
+                Thread.sleep(1000);
+            }
+
+            System.out.println("Dejando de esperar");
+
+            return match;
+        } else {
+            boolean isFull = true;
+
+            for (Match match : Server.matches.values()) {
+                if (match.getGameType().equalsIgnoreCase(gameType) && !match.isMatchFull()) {//si hay una partida con hueco
+                    player.setRoll(1);
+                    modifyMatch(match, player);
+                    isFull = false;
+                    while (!match.isMatchFull()) {
+                        System.out.println("Esperando jugadores2");
+                        //wait();
+                        Thread.sleep(1000);
+                    }
+
+                    return match;
+                }
+            }
+
+            if (isFull) {//si hay partidas pero ninguna con hueco
+                Match match = new Match(plusCount(), gameType, playersNeeded);
+                player.setRoll(0);
+                modifyMatch(match, player);
+                modifyListMatch(match);
+                while (!match.isMatchFull()) {
+                    System.out.println("Esperando jugadores3");
+                    //wait();
+                    Thread.sleep(1000);
+
+                }
+
+                return match;
+            }
+        }
+
+        return null;
+
+    }
+
+    private static synchronized void modifyListMatch(Match match) {
+        Server.matches.put(match.getId(), match);
+
+    }
+
+    private static synchronized void modifyMatch(Match match, Player player) {
+        match.getPlayers().add(player);
+
+    }
+    public static synchronized void endMatch(int id) {
+        Server.matches.remove(id);
+    }
+
+
+    public static synchronized int plusCount() {
+        return idMatch++;
     }
 
 
